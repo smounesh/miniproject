021dcf3fbab54cb574f2eef65e2a912bb726d6f9
smounesh <shankar33cr7@gmail.com>
2024-05-22 17:57:17 +0530

    Added Contexts

---
+++ BankingSystem/BankingSystem/Contexts/BankingSystemContext.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BankingSystem.Models;
+
+namespace BankingSystem.Contexts
+{
+    public class BankingSystemContext : DbContext
+    {
+        public BankingSystemContext(DbContextOptions<BankingSystemContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<Loan> Loans { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Branch> Branches { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // User-Account relationship
+            modelBuilder.Entity<Account>()
+                .HasOne(a => a.User)
+                .WithMany(u => u.Accounts)
+                .HasForeignKey(a => a.UserID)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            // Account-Transaction relationship
+            modelBuilder.Entity<Transaction>()
+                .HasOne(t => t.Account)
+                .WithMany(a => a.Transactions)
+                .HasForeignKey(t => t.AccountID)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Transaction>()
+                .HasOne(t => t.FromAccount)
+                .WithMany()
+                .HasForeignKey(t => t.FromAccountID)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Transaction>()
+                .HasOne(t => t.ToAccount)
+                .WithMany()
+                .HasForeignKey(t => t.ToAccountID)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            // User-Loan relationship
+            modelBuilder.Entity<Loan>()
+                .HasOne(l => l.User)
+                .WithMany(u => u.Loans)
+                .HasForeignKey(l => l.UserID)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            // Employee-User relationship
+            modelBuilder.Entity<Employee>()
+                .HasOne(e => e.User)
+                .WithOne(u => u.Employee)
+                .HasForeignKey<Employee>(e => e.UserID)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            // Employee-Branch relationship
+            modelBuilder.Entity<Employee>()
+                .HasOne(e => e.Branch)
+                .WithMany(b => b.Employees)
+                .HasForeignKey(e => e.BranchID)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            // Account-Branch relationship (created and home branches)
+            modelBuilder.Entity<Account>()
+                .HasOne(a => a.CreatedBranch)
+                .WithMany(b => b.CreatedAccounts)
+                .HasForeignKey(a => a.AccountCreatedBranch)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Account>()
+                .HasOne(a => a.HomeBranch)
+                .WithMany(b => b.HomeAccounts)
+                .HasForeignKey(a => a.CurrentHomeBranch)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
