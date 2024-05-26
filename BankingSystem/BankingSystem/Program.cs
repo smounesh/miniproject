f3d6df5f2e28ef65d1b4e29580e51691b1c00da8
smounesh <shankar33cr7@gmail.com>
2024-05-26 14:08:00 +0530

    Added Serilog

--- BankingSystem/BankingSystem/Program.cs
+++ BankingSystem/BankingSystem/Program.cs
@@ -1,5 +1,6 @@
 using BankingSystem.Contexts;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +12,11 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<BankingSystemContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+#region logging
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .CreateLogger();
+#endregion
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
